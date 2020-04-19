6936007aeafaa5ca777eb0b918d871a4ef9b1ba3
kleveshenrique <kleveshenrique@gmail.com>
2020-04-18 22:34:31 -0300

    conclusão do modulo 4.2

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -9,18 +9,58 @@ appCliente.controller("indexController",function($scope,$http){
 	$scope.nome = "kleves";
 	$scope.sobrenome = "Henrique Aguiar";
 	$scope.clientes=[];
+	$scope.cliente={};
 	
-	$http({
-		method :'GET',
-		url : 'http://localhost:8080/clientes'
-	})
-	.then(function(response){
-		$scope.clientes = response.data;  
-	},function(response){
-		console.log(response.data);
-		console.log(response.status);
+	//metodo para carregar os clientes no scope
+	$scope.carregarClientes = function(){
+		$http({
+			method :'GET',
+			url : 'http://localhost:8080/clientes'
+		})
+		.then(function(response){
+			$scope.clientes = response.data;  
+		},function(response){
+			console.log(response.data);
+			console.log(response.status);
+			
+		});
+	}
+	
+	//$scope.carregarClientes();
+	
+	//método para salvar um cliente e adicionar na lista	
+	$scope.salvarCliente = function(){
 		
-	});
+		$http({
+			method : "POST",
+			url : "http://localhost:8080/clientes",
+			data : $scope.cliente
+		})
+		.then(function(response){
+			$scope.clientes.push(response.data);
+			$scope.cliente.nome="";
+			
+		},function(response){
+			console.log(response.data);
+			console.log(response.status);
+		});
+		
+		// método parqa excluir o cliente 
+		$scope.excluirCliente=function(cliente){
+			$http({
+				
+				method : "DELETE",
+				url : "http://localhost:8080/clientes/"+cliente.id,
+				
+			}).then(function(){
+				var indice = $scope.clientes.indexOf(cliente);
+				$scope.clientes.splice(indice,1);
+			},function(){
+				
+			});
+		}
+		
+	}
 	
 	
 });
