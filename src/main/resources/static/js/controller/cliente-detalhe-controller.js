bf415bbb4afee8fbe53559741c63c978d56a5dde
kleveshenrique <kleveshenrique@gmail.com>
2020-04-23 18:46:17 -0300

    testando o branch do github

--- src/main/resources/static/js/controller/cliente-detalhe-controller.js
+++ src/main/resources/static/js/controller/cliente-detalhe-controller.js
@@ -1,7 +1,7 @@
 appCliente.controller('clienteDetalheController',function($scope,$http,$routeParams){
 	
 	$scope.cliente={};
-	$scope.titulo = "Pagina detalhe do cliente";
+	$scope.titulo = "Pagina detalhe do cliente para teste com o branch - github";
 	
 	$http({
 		method : 'GET',
